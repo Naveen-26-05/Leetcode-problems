1095d774f8458f8226e9b17583c846ec37466c98
numasankar <numasankar@tekion.com>
2024-03-05 23:51:02 +0530

    Get minimum length of string after deleting similar ends

---
+++ prob1750.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int minimumLength(string s) {
+    int i = 0, j = s.size() - 1;
+    char prev_pref = '-';
+    char prev_suf = '-';
+    
+    while (i < j) {
+        if (prev_pref == s[i] || prev_suf == s[j]) {
+            if (prev_pref == s[i]) {
+                prev_pref = s[i];
+                s[i] = '-';
+                i++;
+            }
+            if (prev_suf == s[j]) {
+                prev_suf = s[j];
+                s[j] = '-';
+                j--;
+            }
+            continue;
+        }
+        if (s[i] == s[j]) {
+            prev_pref = s[i];
+            prev_suf = s[j];
+            s[i] = '-';
+            s[j] = '-';
+            j--;
+            i++;
+        } else {
+            break;
+        }
+    }
+    if (i == j) {
+        if (prev_pref == s[i] || prev_suf == s[j]) {
+            return 0;
+        }
+    }
+    return j - i + 1;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    int minLength = minimumLength(input);
+    cout << "Minimum length of the modified string: " << minLength << endl;
+
+    return 0;
+}
